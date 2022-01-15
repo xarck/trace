9c24ea58e86b17b4dfa833f69c918c4b0c188c30
xarck <iamashkin@gmail.com>
2022-01-15 12:26:41 +0530

    server changed

--- server/spotify.js
+++ server/spotify.js
@@ -3,10 +3,6 @@ const express = require("express");
 const request = require("request");
 var app = express();
 
-var client_id = "04ae1192f5524805baa90fbe9b6f43cb";
-var client_secret = "549da1c4b92f4c638128628e10b40153";
-var redirect_uri = "http://localhost:3000/callback";
-
 app.get("/login", function (req, res) {
     var state = "7";
     var scope = "user-read-private user-read-email user-top-read";
@@ -23,16 +19,6 @@ app.get("/login", function (req, res) {
     );
 });
 
-var code =
-    "AQCCHNNAJ9CzCopU7AAo9fkXu6429mWLXjY7dH5cLNP-_wRARr6bdoUyeHce1FttphMZeh_Veq5zoTahuD5PbnSCqR9oNdPjCJj1X_tz6FxBmzoAPHEeazjumNdmF_hRxYlRWKUoCx6lBMS6zXr36Ym6-ZMZK545ahO5jGikYeouhna3EZ0aiDT0rEewpXB3ZflMVVmLN4_yJ9h0t275u1bvYNQu_6Fo7QExUYDzqtUevH-W0Q";
-var c2 =
-    "AQArIgkvIMQwiGhSxnEEVfDxAnJhSHzW2ghTyKAzN2WJq1790qIJ8DLpXmooarYsmoyR9MXOOmr2er7l52If0Tf6RXcnEknXSovC6HJC-gOH9HBSE7j2b0SIBtRgxlQV5Xyoq6BVUFGtCrY96qH9CPCkrdc653zP";
-var refresh_token =
-    "AQAEeNZluzj_SKmfHIzBcnQNIFAqbcpKEQSuwhLtFYS8qGTtsr-Qyyx7TK0paLfl8WCw9KpbwDT-3srGp5I73sIlo4_oWnFEKiG0K_1vOHVmDjstuhBtrv3JUs8XFwHBbkg";
-var access =
-    "BQDI-pflx_wO4UHRZ7r7UbCwbuBmtgh5hT_Wpf77mXlEkmxCZK69upJXXBpu5vCc3Gw4Ygmctn5SxnmcymjdNuDN56xP5cGRjpSG9nM8aHfzVeGvB4fjPa65wNPWBl_3Yd7bLoHTvbnH4pUZLYDEI_1RXWLw_7IMfAL153pfYYzIf6mDQhVe0O2XN_c";
-var ds =
-    "AQBnp4WGSuUApfAnTpU_E00JHY1mMFWzHVMBIyk6dNlFFVSiWdUGfM_DXmq1q7xBoWWEI9XofoasVoK7HOIE9VxoH9gn28gz5omeHSxsUQ5leapa6rYkGgDW1CnOwLsed4mkCA3fgYeWFz9AxWhPtLH7Vea7VSbcJ1hHZF-jzg3NB8mq90BPXgXF92puYTZBUzPhdtstfT7f0EX8A7yMZnL_1lu5jsA0H7rw3jI4FKBXc9ADxA";
 app.get("/callback", function (req, res) {
     console.log(code);
     var code = c2;
